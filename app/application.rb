20b03180f4069ee2c5ffe1102d7aa5a32c4a8264
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-30 20:30:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,10 @@
-class Application 
-  
-  def call(env) 
-    resp = Rack::Response.new 
-    resp.write "Hello, World/n" 
-    resp.finish 
-  end 
-  
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    
+    resp.write "Hello, World\n"
+    resp.finish
+  end
+
 end 
